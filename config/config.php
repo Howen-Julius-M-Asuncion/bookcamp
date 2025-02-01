ae970053507027677ece7a96d4f151009daa9afe
Howen-Julius-M-Asuncion <kaiatmachines@gmail.com>
2025-02-02 01:13:20 +0800

    Project config updated

--- config/config.php
+++ config/config.php
@@ -3,4 +3,9 @@
 define('SITE_NAME', 'CampBook');
 define('BASE_URL', 'http://localhost/webdev2');
 define('FAVICON', BASE_URL . '/public/favicon.ico');
+
+// Enable authentication and admin session variables
+$debugModeAuth = false;
+$debugModeAdmin = true;
+$debugModeUser = 0;
 ?>
