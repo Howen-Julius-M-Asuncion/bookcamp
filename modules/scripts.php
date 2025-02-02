b5df9bba37e1ae6b7ffbdabbefbc6496c02bc639
Howen-Julius-M-Asuncion <kaiatmachines@gmail.com>
2025-02-02 11:40:33 +0800

    Separate styling and script files of modules

---
+++ modules/scripts.php
@@ -0,0 +1,6 @@
+<!-- BS5 -->
+<script src="<?php echo BASE_URL?>/modules/bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo BASE_URL?>/modules/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+
+<!-- Modules -->
+<script src="<?php echo BASE_URL?>/modules/DataTables/datatables.min.js"></script>
