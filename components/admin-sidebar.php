95f8e4d1a4eb742881975e125c9a3de8fa9fbcf7
Howen-Julius-M-Asuncion <kaiatmachines@gmail.com>
2025-02-02 22:39:49 +0800

    Update sidebars with relevant links and styling

--- components/admin-sidebar.php
+++ components/admin-sidebar.php
@@ -1,11 +1,69 @@
 <?php 
-    include_once('../config/config.php');
-    include_once('../modules/styling.php');
+    // include_once('../config/config.php');
+    // include_once('../modules/styling.php');
 ?>
 <style>
     <?php
-        if($_SESSION['currentPage'] == "Home"){ 
-            echo "background-color: var(--gray);";
+        if ($_SESSION['currentPage'] == "Home"){ 
+            echo '
+            .navigation .li-home {
+                background-color: var(--gray);
+            }
+            
+            .navigation .li-home a {
+                color: var(--blue);
+            }
+            ';
+        }else if ($_SESSION['currentPage'] == "Dashboard"){ 
+            echo '
+            .navigation .li-dash {
+                background-color: var(--gray);
+            }
+            
+            .navigation .li-dash a {
+                color: var(--blue);
+            }
+            ';
+        }else if ($_SESSION['currentPage'] == "Users"){ 
+            echo '
+            .navigation .li-users {
+                background-color: var(--gray);
+            }
+            
+            .navigation .li-users a {
+                color: var(--blue);
+            }
+            ';
+        }else if ($_SESSION['currentPage'] == "Sections"){ 
+            echo '
+            .navigation .li-sections {
+                background-color: var(--gray);
+            }
+            
+            .navigation .li-sections a {
+                color: var(--blue);
+            }
+            ';
+        }else if ($_SESSION['currentPage'] == "Courses"){ 
+            echo '
+            .navigation .li-courses {
+                background-color: var(--gray);
+            }
+            
+            .navigation .li-courses a {
+                color: var(--blue);
+            }
+            ';
+        }else if ($_SESSION['currentPage'] == "Departments"){ 
+            echo '
+            .navigation .li-departments {
+                background-color: var(--gray);
+            }
+            
+            .navigation .li-departments a {
+                color: var(--blue);
+            }
+            ';
         }
     ?>
 </style>
@@ -20,22 +78,24 @@
                 <?php 
                     if (isset($_SESSION['authenticated']) == "TRUE" && (isset($_SESSION['userID']))){
                         echo "{$_SESSION['userName']}";
+                    }else{
+                        echo "Auth not setup properly";
                     }
                 ?>
                 </span>
             </a>
         </li>
         <hr>
-        <li>
-            <a href="<?php echo BASE_URL?>">
+        <!-- <li class="li-home">
+            <a href="<?php echo BASE_URL?>/pages/home.php">
                 <span class="icon">
                     <i class="fa-solid fa-house-user"></i>
                 </span>
                 <span class="title">Home</span>
             </a>
-        </li>
-        <li>
-            <a href="<?php echo BASE_URL?>">
+        </li> -->
+        <li class="li-dash">
+            <a href="<?php echo BASE_URL?>/pages/admin/dashboard.php">
                 <span class="icon">
                     <i class="fa-solid fa-chart-line"></i>
                 </span>
@@ -43,36 +103,36 @@
             </a>
         </li>
         <hr>
-        <li>
-            <a href="<?php echo BASE_URL?>">
+        <li class="li-sections">
+            <a href="<?php echo BASE_URL?>/pages/admin/book_inv.php">
+                <span class="icon">
+                    <i class="fa-solid fa-book-open"></i>
+                </span>
+                <span class="title">Book Inventory</span>
+            </a>
+        </li>
+        <li class="li-departments">
+            <a href="<?php echo BASE_URL?>/pages/admin/borrow.php">
+                <span class="icon">
+                    <i class="fa-regular fa-clipboard"></i>
+                </span>
+                <span class="title">Borrow Records</span>
+            </a>
+        </li>
+        <li class="li-users">
+            <a href="<?php echo BASE_URL?>/pages/admin/user_mngmt.php">
                 <span class="icon">
                     <i class="fa-solid fa-users-gear"></i>
                 </span>
-                <span class="title">Users</span>
+                <span class="title">User Management</span>
             </a>
         </li>
-        <li>
-            <a href="<?php echo BASE_URL?>">
+        <li class="li-courses">
+            <a href="<?php echo BASE_URL?>/pages/admin/genre_mngmt.php">
                 <span class="icon">
-                    <i class="fa-solid fa-graduation-cap"></i>
+                    <i class="fa-solid fa-masks-theater"></i>
                 </span>
-                <span class="title">Sections</span>
-            </a>
-        </li>
-        <li>
-            <a href="<?php echo BASE_URL?>">
-                <span class="icon">
-                    <i class="fa-solid fa-book"></i>
-                </span>
-                <span class="title">Courses</span>
-            </a>
-        </li>
-        <li>
-            <a href="<?php echo BASE_URL?>">
-                <span class="icon">
-                    <i class="fa-solid fa-building-user"></i>
-                </span>
-                <span class="title">Departments</span>
+                <span class="title">Genre Management</span>
             </a>
         </li>
         <hr>
@@ -97,4 +157,26 @@
     </div>
 
     <!-- Sidebar Script -->
-    <script src="<?php echo BASE_URL?>/components/js/sidebar.js"></script>
+    <script>
+        // add hovered class to selected list item
+        let list = document.querySelectorAll(".navigation li");
+
+        function activeLink() {
+        list.forEach((item) => {
+            item.classList.remove("hovered");
+        });
+        this.classList.add("hovered");
+        }
+
+        list.forEach((item) => item.addEventListener("mouseover", activeLink));
+
+        // Menu Toggle
+        let toggle = document.querySelector(".toggle");
+        let navigation = document.querySelector(".navigation");
+        let main = document.querySelector(".main");
+
+        toggle.onclick = function () {
+        navigation.classList.toggle("active");
+        main.classList.toggle("active");
+        };
+    </script>
--- components/user-sidebar.php
+++ components/user-sidebar.php
@@ -1,36 +1,114 @@
 <?php 
-    include_once('../config/config.php');
-    include_once('../modules/styling.php');
+    // include_once('../config/config.php');
+    // include_once('../modules/styling.php');
 ?>
-<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://kit.fontawesome.com/71f85e3db5.js" crossorigin="anonymous"></script> -->
+<style>
+    <?php
+        if ($_SESSION['currentPage'] == "Home"){ 
+            echo '
+            .navigation .li-home {
+                background-color: var(--gray);
+            }
+            
+            .navigation .li-home a {
+                color: var(--blue);
+            }
+            ';
+        }else if ($_SESSION['currentPage'] == "Dashboard"){ 
+            echo '
+            .navigation .li-dash {
+                background-color: var(--gray);
+            }
+            
+            .navigation .li-dash a {
+                color: var(--blue);
+            }
+            ';
+        }else if ($_SESSION['currentPage'] == "Users"){ 
+            echo '
+            .navigation .li-users {
+                background-color: var(--gray);
+            }
+            
+            .navigation .li-users a {
+                color: var(--blue);
+            }
+            ';
+        }else if ($_SESSION['currentPage'] == "Sections"){ 
+            echo '
+            .navigation .li-sections {
+                background-color: var(--gray);
+            }
+            
+            .navigation .li-sections a {
+                color: var(--blue);
+            }
+            ';
+        }else if ($_SESSION['currentPage'] == "Courses"){ 
+            echo '
+            .navigation .li-courses {
+                background-color: var(--gray);
+            }
+            
+            .navigation .li-courses a {
+                color: var(--blue);
+            }
+            ';
+        }else if ($_SESSION['currentPage'] == "Departments"){ 
+            echo '
+            .navigation .li-departments {
+                background-color: var(--gray);
+            }
+            
+            .navigation .li-departments a {
+                color: var(--blue);
+            }
+            ';
+        }
+    ?>
+</style>
 <link rel="stylesheet" href="<?php echo BASE_URL?>/components/css/sidebar.css">
-
 <!-- Sidebar -->
 <div class="navigation">
     <ul>
         <li>
-            <a href="#">
+        <a href="#">
                 <span class="icon"><img src="<?php echo FAVICON;?>"></span>
                 <span class="title">
                 <?php 
                     if (isset($_SESSION['authenticated']) == "TRUE" && (isset($_SESSION['userID']))){
                         echo "{$_SESSION['userName']}";
                     }else{
-                        echo "Not Logged In.";
+                        echo "Auth not setup properly";
                     }
                 ?>
                 </span>
             </a>
         </li>
         <hr>
-        <li>
-            <a href="<?php echo BASE_URL?>">
+        <li class="li-home">
+            <a href="<?php echo BASE_URL?>/pages/home.php">
                 <span class="icon">
-                    <i class="fa-solid fa-user-graduate"></i>
+                    <i class="fa-solid fa-house-user"></i>
                 </span>
-                <span class="title">User</span>
+                <span class="title">Profile</span>
+            </a>
+        </li>
+        <hr>
+        <li class="li-dash">
+            <a href="<?php echo BASE_URL?>/pages/search.php">
+                <span class="icon">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </span>
+                <span class="title">Browse Books</span>
+            </a>
+        </li>
+        <li class="li-dash">
+            <a href="<?php echo BASE_URL?>/pages/profile.php">
+                <span class="icon">
+                    <i class="fa-regular fa-address-card"></i>
+                </span>
+                <span class="title">Reserve Books</span>
             </a>
         </li>
         <hr>
@@ -55,4 +133,26 @@
     </div>
 
     <!-- Sidebar Script -->
-    <script src="<?php echo BASE_URL?>/components/js/sidebar.js"></script>
+    <script>
+        // add hovered class to selected list item
+        let list = document.querySelectorAll(".navigation li");
+
+        function activeLink() {
+        list.forEach((item) => {
+            item.classList.remove("hovered");
+        });
+        this.classList.add("hovered");
+        }
+
+        list.forEach((item) => item.addEventListener("mouseover", activeLink));
+
+        // Menu Toggle
+        let toggle = document.querySelector(".toggle");
+        let navigation = document.querySelector(".navigation");
+        let main = document.querySelector(".main");
+
+        toggle.onclick = function () {
+        navigation.classList.toggle("active");
+        main.classList.toggle("active");
+        };
+    </script>
